0c0afcbbaf280b5e99c1ed0ae9678bc8e45c4920
Holger André <4237666+hjoab@users.noreply.github.com>
2018-10-23 14:59:02 -0600

    HelloGridView  in ResourceDesigner2.cs added

---
+++ helloWorld/Resources/Resource.Designer2.cs
@@ -0,0 +1,97 @@
+namespace HelloGridView
+{
+    [System.CodeDom.Compiler.GeneratedCodeAttribute
+       ("Xamarin.Android.Build.Tas ks",
+       "1.0.0.0")]
+    public partial class Resource
+    {
+        static Resource()
+        {
+            global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+        }
+
+        public static void UpdateIdValues() { }
+        public partial class Attribute
+        {
+            static Attribute()
+            {
+                global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+            }
+
+            private Attribute() { }
+        }
+
+        public partial class Drawable
+        {
+            // aapt resource value: 0x7f020000 
+            public const int Icon = 2130837504;
+
+            // aapt resource value: 0x7f020001 
+            public const int img1 = 2130837505;
+
+            // aapt resource value: 0x7f020002 
+            public const int img2 = 2130837506;
+
+            // aapt resource value: 0x7f020003 
+            public const int img3 = 2130837507;
+
+            // aapt resource value: 0x7f020004 
+            public const int img4 = 2130837508;
+
+            // aapt resource value: 0x7f020005 
+            public const int img5 = 2130837509;
+
+            // aapt resource value: 0x7f020006 
+            public const int img6 = 2130837510;
+
+            // aapt resource value: 0x7f020007 
+            public const int img7 = 2130837511;
+
+            static Drawable()
+            {
+                global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+            }
+
+            private Drawable() { }
+        }
+
+        public partial class Id
+        {
+            // aapt resource value: 0x7f050000 
+            public const int gridview = 2131034112;
+
+            static Id()
+            {
+                global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+            }
+
+            private Id() { }
+        }
+
+        public partial class Layout
+        {
+            // aapt resource value: 0x7f030000 
+            public const int Main = 2130903040;
+            static Layout()
+            {
+                global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+            }
+            private Layout() { }
+        }
+
+        public partial class String
+        {
+            // aapt resource value: 0x7f040001 
+            public const int ApplicationName = 2130968577;
+
+            // aapt resource value: 0x7f040000 
+            public const int Hello = 2130968576;
+
+            static String()
+            {
+                global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+            }
+            private String() { }
+        }
+    }
+}
