691adde94753f4c3bec0568597abdb421505c1ea
Holger André <4237666+hjoab@users.noreply.github.com>
2018-10-22 18:02:59 -0600

    my 1st xamarin app run

--- helloWorld/MainActivity.cs
+++ helloWorld/MainActivity.cs
@@ -1,19 +1,44 @@
-﻿using Android.App;
+﻿//using Android.App;
+//using Android.OS;
+//using Android.Support.V7.App;
+//using Android.Runtime;
+//using Android.Widget;
+
+//namespace helloWorld
+//{
+//    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+//    public class MainActivity : AppCompatActivity
+//    {
+//        protected override void OnCreate(Bundle savedInstanceState)
+//        {
+//            base.OnCreate(savedInstanceState);
+//            // Set our view from the "main" layout resource
+//            SetContentView(Resource.Layout.activity_main);
+//        }
+//    }
+//}
+
+
+using System; 
+using Android.App; 
+using Android.Content; 
+using Android.Runtime; 
+using Android.Views; 
+using Android.Widget; 
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
-using Android.Widget;
 
+
+//namespace HelloXamarin
 namespace helloWorld
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(MainLauncher = true)]
+    public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
         }
     }
 }
