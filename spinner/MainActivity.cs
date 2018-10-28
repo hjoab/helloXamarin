e33655a2b6d1ddcb66e435dd3546d558df60adea
Holger André <4237666+hjoab@users.noreply.github.com>
2018-10-28 14:21:44 -0600

    spinner added and some pretty things done

---
+++ spinner/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace spinner
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Spinner);
+
+            Spinner spinnerDays = FindViewById<Spinner>(Resource.Id.spinner1);
+            spinnerDays.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(SelectedDay); // ojo envia un delagado con igual parametros requeiridos ???? -hjoab
+            var adapter = ArrayAdapter.CreateFromResource(this,
+               Resource.Array.days_array, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinnerDays.Adapter = adapter;
+        }
+        private void SelectedDay(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+            string toast = string.Format("The selected day is {0} ", spinner.GetItemAtPosition(e.Position)); 
+           Toast.MakeText(this, toast, ToastLength.Long).Show();
+        }
+    }
+}
